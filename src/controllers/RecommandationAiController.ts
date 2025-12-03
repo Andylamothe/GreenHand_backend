7f881f9c89bd5cb72d76e562ea2352d0ad52afd9
Andy <Andylamothe22@hotmail.com>
2025-12-03 12:19:20 -0500

    Ajout du squelette des endpoints et des méthodes pour gérer les recommandations basées sur l'IA

---
+++ src/controllers/RecommandationAiController.ts
@@ -0,0 +1,1 @@
+// Endpoint pour gérer les recommandations basées sur l'IA
---
+++ src/services/RecommandationAiService.ts
@@ -0,0 +1,4 @@
+/* Méthodes pour post les méthodes de recommandation basées sur l'IA
+    et les post et get dans la base de donnée MongoDB
+*/
+
